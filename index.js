f9c2e4df2d87c2606334e5b55640db35e9cac0c7
Timothy <tkkibet13@gmail.com>
2022-08-12 00:51:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger')
+    dodger.style.left = "10px";    
+}
+function moveDodgerRight() {
+    const dodger = document.getElementById('dodger')
+    dodger.style.left = "190px";       
+}
